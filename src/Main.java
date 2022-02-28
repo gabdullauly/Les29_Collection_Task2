71de4e36d751415ab7dc7ceb0defa517e465997a
gabdullauly <52780613+gabdullauly@users.noreply.github.com>
2022-02-28 22:07:32 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Human> humans = new ArrayList<>();
+        humans.add(new User("Aisana", 4, "Ais", "Ais123"));
+        humans.add(new User("Ainazar", 1, "Aino", "Aino123"));
+
+
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,33 @@
+public class User extends Human{
+    private String username;
+    private String password;
+
+    public User (){}
+    public User (String name, int age, String username, String password){
+        super(name, age);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername (){
+        return username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    @Override
+    public void printData(){
+        System.out.print(getName()+" "+getAge()+" "+username+" "+password);
+    }
+
+}
